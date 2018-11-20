55a32868a8f36c43d57d30095d21b0465980bef8
Sárik Attila <sarik.attila@gmail.com>
2018-11-20 17:00:42 +0100

    navbar-nav  minusz navbar

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -21,33 +21,33 @@
         </button>
         <div class="collapse navbar-collapse"
              id="navbarTogglerDemo02">
-            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+            <ul class="nav mr-auto mt-2 mt-lg-0">
                 <li class="nav-item px-3">
-                    <a class="nav-link active"
+                    <a class="nav-link"
                        href="#about">
                         <i class="fa fa-drivers-license"></i>
                         Rólam</a>
                 </li>
                 <li class="nav-item px-3">
-                    <a class="nav-link active"
+                    <a class="nav-link"
                        href="#service">
                         <i class="fa fa-pencil-square-o"></i>
                         Szolgáltatások</a>
                 </li>
                 <li class="nav-item px-3">
-                    <a class="nav-link active"
+                    <a class="nav-link"
                        href="#portfolio">
                         <i class="fa fa-th"></i>
                         Munkáim</a>
                 </li>
                 <li class="nav-item px-3">
-                    <a class="nav-link active"
+                    <a class="nav-link"
                        href="#opinion">
                         <i class="fa fa-comments"></i>
                         Vélemények</a>
                 </li>
                 <li class="nav-item px-3">
-                    <a class="nav-link active"
+                    <a class="nav-link"
                        href="#contact">
                         <i class="fa fa-envelope"></i>
                         Kapcsolat</a>
